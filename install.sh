f0af5a40c4f976526728a0819494b60472456ca2
kjwon15 <kjwonmail@gmail.com>
2015-07-08 14:35:43 +0900

    Fix install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-path=`dirname $(realpath $0)`
+path=`dirname $(readlink -e $0)`
 cd $path
 git submodule update --init
 
-files=('cake' 'lie' '.bash_profile' '.gitconfig' '.ideavimrc' '.oh-my-zsh' '.pentadactylrc' '.tmux.conf' '.vim' '.vimrc' '.zshrc')
+files=('.bash_profile' '.gitconfig' '.ideavimrc' '.oh-my-zsh' '.pentadactylrc' '.tmux.conf' '.vim' '.vimrc' '.zshrc')
 
 for file in ${files[@]}; do
     ln -sf $path/$file ~/$file
