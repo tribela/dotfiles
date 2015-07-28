4fa85ab75f9c8119c4c2df7f74a408d61bef2a27
kjwon15 <kjwonmail@gmail.com>
2015-07-28 10:25:44 +0900

    Rename bash_profile to bashrc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ path=`dirname $(readlink -e $0)`
 cd $path
 git submodule update --init
 
-files=('.bash_profile' '.gitconfig' '.ideavimrc' '.oh-my-zsh' '.pentadactylrc' '.tmux.conf' '.vim' '.vimrc' '.zshrc')
+files=('.bashrc' '.gitconfig' '.ideavimrc' '.oh-my-zsh' '.pentadactylrc' '.tmux.conf' '.vim' '.vimrc' '.zshrc')
 
 for file in ${files[@]}; do
     ln -sf $path/$file ~/$file
