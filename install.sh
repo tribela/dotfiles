475836bbe31b7a45e2e7d595eabe495689fdb981
kjwon15 <kjwonmail@gmail.com>
2016-08-22 16:52:14 +0900

    Install oh my zsh correctly

--- install.sh
+++ install.sh
@@ -3,15 +3,18 @@ path=`dirname $(readlink -e $0)`
 cd $path
 git submodule update --init
 
-files=('.bashrc' '.gitconfig' '.ideavimrc' '.oh-my-zsh' '.pentadactylrc' '.tmux.conf' '.vim' '.vimrc' '.zshrc')
+files=('.bashrc' '.gitconfig' '.ideavimrc' '.pentadactylrc' '.tmux.conf' '.vim' '.vimrc' '.zshrc')
 
 for file in ${files[@]}; do
     ln -sf $path/$file ~/$file
 done
 
-./.fzf/install
 
 vim +PlugInstall +qall
 
 mkdir -p "$HOME/.local/bin"
 ln -sf $path/tmx $HOME/.local/bin/tmx
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+./.fzf/install
