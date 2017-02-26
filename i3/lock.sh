bb347687466beaaffa070c7c00ab8461c56e53e0
kjwon15 <kjwonmail@gmail.com>
2017-02-27 01:46:18 +0900

    Enhance lock script

--- i3/lock.sh
+++ i3/lock.sh
@@ -7,5 +7,17 @@ if [ ! -e $here/lock.png ]; then
     convert $here/lock.jpg -resize $size $here/lock.png
 fi
 
-i3lock -c a939ff -i $here/lock.png
+revert() {
+    xset dpms 0 0 0
+}
+
+trap revert SIGINT SIGHUP SIGTERM
+
+xset +dpms dpms 5 5 5
+i3lock -n -c a939ff -i $here/lock.png &
+pid=$!
+sleep 1
 xset dpms force off
+wait $pid
+
+revert
