3846cd5ca7ea31cafe20b9b8636184fba8b11384
kjwon15 <kjwonmail@gmail.com>
2017-03-30 16:52:20 +0900

    Append "--all" option to fzf install

--- install.sh
+++ install.sh
@@ -29,4 +29,4 @@ ln -sf $path/tmx $HOME/.local/bin/tmx
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-$HOME/.fzf/install
+$HOME/.fzf/install --all
