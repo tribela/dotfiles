aac86f7203c3a58141acfe0068f2ff96c62aba51
kjwon15 <kjwonmail@gmail.com>
2017-04-05 18:36:07 +0900

    Apply shellcheck

--- install.sh
+++ install.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-path=`dirname $(readlink -e $0)`
-cd $path
+path=$(dirname "$(readlink -e "$0")")
+cd "$path" || true
 
 files=('.bashrc' '.gitconfig' '.ideavimrc' '.pentadactylrc' '.tmux.conf' '.vim' '.vimrc' '.zshrc')
 
-for file in ${files[@]}; do
-    ln -sfT $path/$file ~/$file
+for file in "${files[@]}"; do
+    ln -sfT "$path/$file" "$HOME/$file"
 done
 
-ln -sfT $path/i3 ~/.config/i3
-ln -sfT $path/i3status ~/.config/i3status
+ln -sfT "$path/i3 ~/.config/i3"
+ln -sfT "$path/i3status" ~/.config/i3status
 
-\curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+'curl' -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugUpgrade +PlugInstall +qall
 
 if hash nvim 2>/dev/null; then
     mkdir -p ~/.config/nvim
-    \curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    'curl' -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    ln -sf $path/.vimrc ~/.config/nvim/init.vim
-     ln -sfT $path/.vim/snippets ~/.config/nvim/snippets
+    ln -sf "$path/.vimrc" ~/.config/nvim/init.vim
+     ln -sfT "$path/.vim/snippets" ~/.config/nvim/snippets
     nvim +PlugUpgrade +PlugInstall +qall
 fi
 
 mkdir -p "$HOME/.local/bin"
-ln -sf $path/tmx $HOME/.local/bin/tmx
+ln -sf "$path/tmx" "$HOME/.local/bin/tmx"
 
 # Install vcprompt
 
@@ -34,7 +34,7 @@ chmod +x ~/.local/bin/vcprompt
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-$HOME/.fzf/install --all
+"$HOME/.fzf/install" --all
 
 # Install ccsm profile
 # Use abs path to ignore pyenv
