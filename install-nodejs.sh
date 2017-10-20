30e15966ca6a9597f9bc09ecdfc669bd9f67a540
kjwon15 <kjwonmail@gmail.com>
2017-10-20 22:38:30 +0900

    Install nodejs 8.x

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sudo bash < <(curl -sL https://deb.nodesource.com/setup_6.x)
+sudo bash < <(curl -sL https://deb.nodesource.com/setup_8.x)
 sudo apt install nodejs
 
 npm install -g add-local-binaries-to-path
