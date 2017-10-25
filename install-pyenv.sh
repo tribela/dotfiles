d64db31c79f242a3f624983330bea4080f04d7ea
kjwon15 <kjwonmail@gmail.com>
2017-10-25 13:32:27 +1000

    Install ncurses for pyenv

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5 libncurses5-dev libncursesw5
 
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
