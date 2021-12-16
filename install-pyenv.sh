08677bca9961ff6167f986d3015bbf94ef8d63aa
kjwon15 <kjwonmail@gmail.com>
2021-12-16 21:21:25 +0900

    Update pyenv install script

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5 libncurses5-dev libncursesw5 tk-dev
+sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+curl -sSL https://pyenv.run | bash
 
 git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
-git clone https://github.com/concordusapps/pyenv-implict.git ~/.pyenv/plugins/pyenv-implict
+# git clone https://github.com/concordusapps/pyenv-implict.git ~/.pyenv/plugins/pyenv-implict
