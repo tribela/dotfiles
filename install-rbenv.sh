47dbdb9e8be220409e47308313b630f17551f838
Jeong Arm <kjwonmail@gmail.com>
2022-03-17 21:13:43 +0900

    Update rbenv install script

--- install-rbenv.sh
+++ install-rbenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/rbenv/rbenv ~/.rbenv
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
 mkdir -p ~/.rbenv/plugins
 git clone https://github.com/rbenv/ruby-build ~/.rbenv/plugins/ruby-build
+
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | bash
