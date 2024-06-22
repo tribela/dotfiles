4f4cdc70b3687e6ad8196a22f10cb01c447617a1
Jeong Arm <kjwonmail@gmail.com>
2024-06-22 15:48:34 +0900

    Enable ALE

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -5,9 +5,7 @@ return {
 		local keymap = vim.keymap
 
 		g.ale_completion_enabled = 1
-		-- g.ale_linters = {
-		-- 	python = { 'flake8' },
-		-- }
+		g.ale_use_neovim_diagnostics_api = 1
 
 		keymap.set('n', '[x', '<Plug>(ale_previous_wrap)')
 		keymap.set('n', ']x', '<Plug>(ale_next_wrap)')
