8026fdda295fb0c20adbb62634d2993fd9c5593c
Jeong Arm <kjwonmail@gmail.com>
2024-06-26 11:17:47 +0900

    Fix ibl trailing spaces

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,8 +1,31 @@
 return {
-	'lukas-reineke/indent-blankline.nvim',
-	event = { 'BufReadPre', 'BufNewFile'},
-	main = 'ibl',
-	opts = {
-		indent = { char = '┆' },
-	},
+  'lukas-reineke/indent-blankline.nvim',
+  event = { 'BufReadPre', 'BufNewFile'},
+  main = 'ibl',
+  config = function()
+    ibl = require('ibl')
+
+    local highlight = {
+      "ModeMsg",
+    }
+
+    vim.opt.list = true
+    vim.opt.listchars = {
+      tab = '┆ ',
+      trail = '•',
+      nbsp = '␣',
+    }
+
+    ibl.setup {
+      indent = {
+        char = '┆',
+      },
+      whitespace = {
+        remove_blankline_trail = false,
+      },
+      scope = {
+        highlight = highlight
+      },
+    }
+  end
 }
