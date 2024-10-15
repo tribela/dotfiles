e74a5319fa1eede502f401a4dc0100a6b31f764c
Jeong Arm <kjwonmail@gmail.com>
2024-10-16 00:12:25 +0900

    [Nvim] Ensure LSP and lints are installed

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -2,11 +2,13 @@ return {
 	'williamboman/mason.nvim',
 	dependencies = {
 		'williamboman/mason-lspconfig.nvim',
+		'WhoIsSethDaniel/mason-tool-installer.nvim',
 	},
 	lazy = true,
 	config = function()
 		local mason = require('mason')
 		local mason_lspconfig = require('mason-lspconfig')
+		local mason_tool_installer = require('mason-tool-installer')
 
 		mason.setup({
 			ui = {
@@ -20,8 +22,19 @@ return {
 
 		mason_lspconfig.setup({
 			ensure_installed = {
-				'tsserver',
+				'bashls',
+				'ts_ls',
+				'lua_ls',
 				'html',
+				'pylsp',
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				'shellcheck',
+				'flake8',
+				'mypy',
 				'pyright',
 			},
 		})
